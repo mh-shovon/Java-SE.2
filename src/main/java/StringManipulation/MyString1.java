22ab1da5a0f3b5ba7df6c2bb7d7bf84d49de94d7
MEHEDI HASAN SHOVON <shovon33266@gmail.com>
2022-08-20 02:02:05 +0600

    New work on my String

--- src/main/java/StringManipulation/MyString1.java
+++ src/main/java/StringManipulation/MyString1.java
@@ -4,5 +4,6 @@ public class MyString1 {
     public static void main(String[] args) {
         String str = " I am Mehedi Hasan Shovon";
         System.out.println(str);
+        System.out.println("hello my love");
     }
 }
